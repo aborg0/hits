30ce02fb2f76504560d564367499fba3857cb807
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2008-12-11 12:03:20 +0000

    Adding the other bugfixes.

--- ie.tcd.imm.hits/tags/version_0.0.1/ie.tcd.imm.hits/src/ie/tcd/imm/hits/knime/ranking/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes in this package makes it possible to rank the data
- * based on the order of the values for some parameters.
- */
-package ie.tcd.imm.hits.knime.ranking;
--- ie.tcd.imm.hits/tags/version_0.0.1/ie.tcd.imm.hits/src/ie/tcd/imm/hits/knime/util/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The classes in this package are general purpose,
- * KNIME related classes.
- */
-package ie.tcd.imm.hits.knime.util;
