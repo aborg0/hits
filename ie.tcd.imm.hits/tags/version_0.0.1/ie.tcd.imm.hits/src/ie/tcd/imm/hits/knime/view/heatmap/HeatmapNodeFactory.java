2dd0d446d19840d3084d8bf10b7098ca4c8fb9f0
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2008-12-11 11:38:52 +0000

    Update of tag to represent the bug fixes.

---
+++ ie.tcd.imm.hits/tags/version_0.0.1/ie.tcd.imm.hits/src/ie/tcd/imm/hits/knime/view/heatmap/HeatmapNodeFactory.java
@@ -0,0 +1,58 @@
+package ie.tcd.imm.hits.knime.view.heatmap;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "Heatmap" Node.
+ * Shows the heatmap of the plates.
+ *
+ * @author <a href="mailto:bakosg@tcd.ie">Gabor Bakos</a>
+ */
+public class HeatmapNodeFactory extends NodeFactory {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeModel createNodeModel() {
+        return new HeatmapNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView createNodeView(final int viewIndex,
+            final NodeModel nodeModel) {
+        return new HeatmapNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new HeatmapNodeDialog();
+    }
+
+}
+
---
+++ ie.tcd.imm.hits/tags/version_0.0.1/ie.tcd.imm.hits/src/ie/tcd/imm/hits/knime/view/heatmap/InfoControl.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package ie.tcd.imm.hits.knime.view.heatmap;
+
+import ie.tcd.imm.hits.knime.view.heatmap.HeatmapNodeModel.StatTypes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serializable;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+
+/**
+ * Sets the information format for the associated information {@link JTable}s.
+ * 
+ * @author <a href="mailto:bakosg@tcd.ie">Gabor Bakos</a>
+ */
+public class InfoControl extends JPanel {
+	private static final long serialVersionUID = -6252559407229787836L;
+
+	private static final String DEFAULT_PATTERN = "<html>\nPlate: <b>${plate}</b> Well: <b>${well}</b>"
+			+ " Normalisation: ${Normalisation method} - ${Normalisation kind}<table>\n"
+			+ "${"
+			+ StatTypes.score.name()
+			+ "}</table>\n<table>${"
+			+ StatTypes.normalized.name() + "}</table>\n</html>";
+
+	private final JButton updateButton = new JButton("update");
+	private final JTextArea text = new JTextArea(DEFAULT_PATTERN, 20, 100);
+
+	private ViewModel viewModel;
+	private final LabelPatternUpdater actionListener = new LabelPatternUpdater();
+
+	private final class LabelPatternUpdater implements ActionListener,
+			Serializable {
+		private static final long serialVersionUID = -2841593296782424832L;
+
+		/**
+		 * Updates the {@link InfoControl#viewModel}'s label pattern.
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void actionPerformed(final ActionEvent e) {
+			viewModel.setLabelPattern(text.getText());
+		}
+	}
+
+	/**
+	 * Constructs an {@link InfoControl} object to be able to influence the
+	 * labels of wells.
+	 */
+	public InfoControl() {
+		super();
+		add(updateButton);
+		add(new JScrollPane(text));
+		updateButton.addActionListener(actionListener);
+	}
+
+	/**
+	 * Updating the {@link ViewModel} to modify.
+	 * 
+	 * @param viewModel
+	 *            The new {@link ViewModel}.
+	 */
+	public void setViewModel(final ViewModel viewModel) {
+		this.viewModel = viewModel;
+		viewModel.setLabelPattern(text.getText());
+	}
+}
