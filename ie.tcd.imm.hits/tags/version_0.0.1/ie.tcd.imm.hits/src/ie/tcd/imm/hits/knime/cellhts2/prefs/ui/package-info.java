bc756af003f754d83abad9046777765d8d526986
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2008-12-11 11:35:36 +0000

    Removed to update the version_0.0.1 tag.

--- ie.tcd.imm.hits/tags/version_0.0.1/ie.tcd.imm.hits/src/ie/tcd/imm/hits/knime/cellhts2/prefs/ui/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The classes in this package are for the custom user interface of
- * the general preferences of the cellHTS2 node.
- */
-package ie.tcd.imm.hits.knime.cellhts2.prefs.ui;
--- ie.tcd.imm.hits/tags/version_0.0.1/ie.tcd.imm.hits/src/ie/tcd/imm/hits/util/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Classes in this package are general purpose utility classes.
- */
-package ie.tcd.imm.hits.util;
