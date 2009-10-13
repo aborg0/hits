98ef6560ed74778034324bf3c437dc00f807ac68
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2009-10-13 13:47:13 +0000

    Tagging version 0.4.1

---
+++ ie.tcd.imm.hits.exp/tags/version_0.4.1/ie.tcd.imm.hits.exp/src/ie/tcd/imm/hits/knime/util/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
+/**
+ * The classes in this package are sorting helping other classes in this project.
+ */
+@DefaultAnnotation( { Nonnull.class, CheckReturnValue.class })
+package ie.tcd.imm.hits.knime.util;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+
