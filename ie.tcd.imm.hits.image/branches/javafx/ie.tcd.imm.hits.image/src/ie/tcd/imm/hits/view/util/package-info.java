0b7cc85b0109044e220e29bf09be7d60d48c27ab
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2009-11-10 14:44:32 +0000

    Starting a new branch to experiment with a JavaFX user interface.

---
+++ ie.tcd.imm.hits.image/branches/javafx/ie.tcd.imm.hits.image/src/ie/tcd/imm/hits/view/util/package-info.java
@@ -0,0 +1,13 @@
+/* All rights reserved. (C) Copyright 2009, Trinity College Dublin */
+
+/**
+ * Some useful classes for visual representations.
+ */
+@DefaultAnnotation( { CheckReturnValue.class, Nonnull.class })
+package ie.tcd.imm.hits.view.util;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+
