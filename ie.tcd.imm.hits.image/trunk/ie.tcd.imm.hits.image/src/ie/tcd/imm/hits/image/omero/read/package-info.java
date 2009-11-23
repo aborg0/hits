890449d1bf028fe1035cbfd2444c01a2b102a79f
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2009-11-23 17:19:07 +0000

    Intial commit of the OMERO support, it does nothing yet, only allows a way to start the services of OMERO Insight. (The referenced dependencies are not committed, testOmero.jar was generated by the test sources of InsightTrunkMain project.)

---
+++ ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image/src/ie/tcd/imm/hits/image/omero/read/package-info.java
@@ -0,0 +1,13 @@
+/* All rights reserved. (C) Copyright 2009, Trinity College Dublin*/
+
+/**
+ * 
+ */
+@DefaultAnnotation( { CheckReturnValue.class, Nonnull.class })
+package ie.tcd.imm.hits.image.omero.read;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+
