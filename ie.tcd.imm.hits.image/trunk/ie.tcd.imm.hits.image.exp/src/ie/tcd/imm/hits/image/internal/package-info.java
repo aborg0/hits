be5a62cac6fd36f422894dc46004ce7e07ca2dd1
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2009-12-04 15:14:04 +0000

    Moved the OMERO client related code to the imaging experimental project.

---
+++ ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image.exp/src/ie/tcd/imm/hits/image/internal/package-info.java
@@ -0,0 +1,9 @@
+/**All rights reserved. (C) Copyright 2009, Trinity College Dublin*/
+
+@DefaultAnnotation({CheckReturnValue.class, Nonnull.class})
+package ie.tcd.imm.hits.image.internal;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
---
+++ ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image.exp/src/ie/tcd/imm/hits/image/omero/OmeroValue.java
@@ -0,0 +1,15 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
+package ie.tcd.imm.hits.image.omero;
+
+import org.knime.core.data.DataValue;
+
+/**
+ * An interface representing an image on OMERO server.
+ * 
+ * @author <a href="mailto:bakosg@tcd.ie">Gabor Bakos</a>
+ */
+public interface OmeroValue extends DataValue {
+
+}
