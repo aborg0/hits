d76168d0174fa1aa0dd6637eb3173472813d9f47
aborgabor <aborgabor@15a556d8-b8bf-11dd-9d36-1d0c09c95ed7>
2009-12-19 20:22:12 +0000

    Small enhancements for the dialog.

--- ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image.exp/src/ie/tcd/imm/hits/image/omero/read/OMEROReaderNodeDialog.java
+++ ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image.exp/src/ie/tcd/imm/hits/image/omero/read/OMEROReaderNodeDialog.java
@@ -1,15 +1,42 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
 package ie.tcd.imm.hits.image.omero.read;
 
 import ie.tcd.imm.hits.image.omero.read.OMEROReaderNodeModel.ConnectionSpeed;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentButton;
 import org.knime.core.node.defaultnodesettings.DialogComponentNumberEdit;
 import org.knime.core.node.defaultnodesettings.DialogComponentPasswordField;
 import org.knime.core.node.defaultnodesettings.DialogComponentString;
+import org.knime.core.node.defaultnodesettings.DialogComponentStringListSelection;
 import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelEnum;
 import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
+import org.knime.core.node.defaultnodesettings.UpdatableComponent;
+import org.knime.core.util.KnimeEncryption;
+import org.openmicroscopy.shoola.env.data.DSAccessException;
+import org.openmicroscopy.shoola.env.data.DSOutOfServiceException;
+import org.openmicroscopy.shoola.env.data.OmeroDataService;
+
+import pojos.ExperimenterData;
+import pojos.ImageData;
 
 /**
  * <code>NodeDialog</code> for the "OMEROReader" Node. Allows to import data
@@ -31,28 +58,143 @@ public class OMEROReaderNodeDialog extends DefaultNodeSettingsPane {
 		super();
 		createNewGroup("server");
 		setHorizontalPlacement(true);
-		addDialogComponent(new DialogComponentString(new SettingsModelString(
+		final SettingsModelString serverNameModel = new SettingsModelString(
 				OMEROReaderNodeModel.CFGKEY_SERVER_NAME,
-				OMEROReaderNodeModel.DEFAULT_SERVER_NAME), "Server: "));
-		addDialogComponent(new DialogComponentNumberEdit(
-				new SettingsModelIntegerBounded(
-						OMEROReaderNodeModel.CFGKEY_SERVER_PORT,
-						OMEROReaderNodeModel.DEFAULT_SERVER_PORT, 0, 65535),
-				"Port: "));
+				OMEROReaderNodeModel.DEFAULT_SERVER_NAME);
+		addDialogComponent(new DialogComponentString(serverNameModel,
+				"Server: "));
+		final SettingsModelIntegerBounded portModel = new SettingsModelIntegerBounded(
+				OMEROReaderNodeModel.CFGKEY_SERVER_PORT,
+				OMEROReaderNodeModel.DEFAULT_SERVER_PORT, 0, 65535);
+		addDialogComponent(new DialogComponentNumberEdit(portModel, "Port: "));
 		final SettingsModelEnum<ConnectionSpeed> connectionSpeedModel = new SettingsModelEnum<ConnectionSpeed>(
 				OMEROReaderNodeModel.CFGKEY_CONNECTION_SPEED,
-				OMEROReaderNodeModel.DEFAULT_CONNECTION_SPEED, ConnectionSpeed
-						.values());
+				OMEROReaderNodeModel.DEFAULT_CONNECTION_SPEED,
+				OMEROReaderNodeModel.POSSIBLE_CONNECTION_SPEEDS);
 		addDialogComponent(new DialogComponentStringSelection(
 				connectionSpeedModel, "Connection speed: ",
 				connectionSpeedModel.getDisplayTexts()));
 		createNewGroup("user");
 		setHorizontalPlacement(false);
-		addDialogComponent(new DialogComponentString(new SettingsModelString(
+		final SettingsModelString usernameModel = new SettingsModelString(
 				OMEROReaderNodeModel.CFGKEY_USERNAME,
-				OMEROReaderNodeModel.DEFAULT_USERNAME), "User name: "));
-		addDialogComponent(new DialogComponentPasswordField(
-				new SettingsModelString(OMEROReaderNodeModel.CFGKEY_PASSWORD,
-						OMEROReaderNodeModel.DEFAULT_PASSWORD), "Password: "));
+				OMEROReaderNodeModel.DEFAULT_USERNAME);
+		addDialogComponent(new DialogComponentString(usernameModel,
+				"User name: "));
+		final SettingsModelString passwordModel = new SettingsModelString(
+				OMEROReaderNodeModel.CFGKEY_PASSWORD,
+				OMEROReaderNodeModel.DEFAULT_PASSWORD);
+		addDialogComponent(new DialogComponentPasswordField(passwordModel,
+				"Password: "));
+		final SettingsModelStringArray optionsModel = new SettingsModelStringArray(
+				OMEROReaderNodeModel.CFGKEY_SELECTION,
+				OMEROReaderNodeModel.DEFAULT_SELECTION);
+		createNewGroup("Plates");
+		final DialogComponentStringListSelection optionComponent = new DialogComponentStringListSelection(
+				optionsModel, "Dataset", Collections.singletonList(""), true,
+				10);
+		addDialogComponent(optionComponent);
+		addDialogComponent(new UpdatableComponent() {
+			@Override
+			protected void updateComponent()/* => */{
+				try {
+					reconnect(serverNameModel.getStringValue(), portModel
+							.getIntValue(), usernameModel.getStringValue(),
+							KnimeEncryption.decrypt(passwordModel
+									.getStringValue()), connectionSpeedModel
+									.getEnumValue(), optionComponent);
+				} catch (final InvalidKeyException e) {
+					OMEROReaderNodeModel.logger.error(e);
+				} catch (final BadPaddingException e) {
+					OMEROReaderNodeModel.logger.error(e);
+				} catch (final IllegalBlockSizeException e) {
+					OMEROReaderNodeModel.logger.error(e);
+				} catch (final UnsupportedEncodingException e) {
+					OMEROReaderNodeModel.logger.error(e);
+				} catch (final IOException e) {
+					OMEROReaderNodeModel.logger.error(e);
+				}
+			}
+		});
+		final ActionListener listener = new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				try {
+					reconnect(serverNameModel.getStringValue(), portModel
+							.getIntValue(), usernameModel.getStringValue(),
+							KnimeEncryption.decrypt(passwordModel
+									.getStringValue()), connectionSpeedModel
+									.getEnumValue(), optionComponent);
+				} catch (final InvalidKeyException e1) {
+					OMEROReaderNodeModel.logger.error(e1);
+				} catch (final BadPaddingException e1) {
+					OMEROReaderNodeModel.logger.error(e1);
+				} catch (final IllegalBlockSizeException e1) {
+					OMEROReaderNodeModel.logger.error(e1);
+				} catch (final UnsupportedEncodingException e1) {
+					OMEROReaderNodeModel.logger.error(e1);
+				} catch (final IOException e1) {
+					OMEROReaderNodeModel.logger.error(e1);
+				}
+			}
+		};
+		// serverNameModel.addChangeListener(listener);
+		// portModel.addChangeListener(listener);
+		// connectionSpeedModel.addChangeListener(listener);
+		// usernameModel.addChangeListener(listener);
+		// passwordModel.addChangeListener(listener);
+		final DialogComponentButton connectButton = new DialogComponentButton(
+				"Connect");
+		connectButton.addActionListener(listener);
+		addDialogComponent(connectButton);
+	}
+
+	protected static void reconnect(final String server, final int port,
+			final String user, final String password,
+			final ConnectionSpeed speed,
+			final DialogComponentStringListSelection optionComponent) {
+		final OMEROHandler handler = OMEROHandler.getInstance();
+		handler.reconnect(server, port, user, password, speed);
+		final OmeroDataService dataService = handler.getDataService();
+		try {
+			final ExperimenterData experimenterData = handler
+					.getExperimenterData();
+			final long userId = experimenterData.getId();
+			final Set<?> experimenterImages = dataService
+					.getExperimenterImages(userId);
+			final SortedSet<String> names = new TreeSet<String>();
+			for (final Object object : experimenterImages) {
+				if (object instanceof ImageData) {
+					final ImageData data = (ImageData) object;
+					// System.out.println("name: " + data.getName());
+					// final Collection<?> containerPaths = dataService
+					// .findContainerPaths(PlateData.class, data.getId(),
+					// -1);
+					// System.out.println(containerPaths);
+					// for (final Object o : containerPaths) {
+					// if (o instanceof DatasetData) {
+					// final DatasetData dataset = (DatasetData) o;
+					// names.add(String.format("%d\u00a0%s", dataset
+					// .getId(), dataset.getName()));
+					// }
+					// }
+					// System.out.println("acqu: " + data.getAcquisitionDate());
+					names.add(String.format("%5d\u00a0%s", data.getId(), data
+							.getName()));
+				}
+			}
+			optionComponent.replaceListItems(names);
+			// final Object advancedSearchFor = dataService
+			// .advancedSearchFor(new SearchDataContext(Collections
+			// .<Integer> emptyList(), Collections
+			// .<Class> singletonList(Dataset.class),
+			// new String[0], new String[0], new String[0]));
+			// System.out.println(advancedSearchFor);
+			System.out.println();
+		} catch (final DSOutOfServiceException e) {
+			OMEROReaderNodeModel.logger.info(e.getMessage(), e);
+		} catch (final DSAccessException e) {
+			OMEROReaderNodeModel.logger.info(e.getMessage(), e);
+		}
 	}
 }
--- ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image.exp/src/ie/tcd/imm/hits/image/omero/read/OMEROReaderNodeModel.java
+++ ie.tcd.imm.hits.image/trunk/ie.tcd.imm.hits.image.exp/src/ie/tcd/imm/hits/image/omero/read/OMEROReaderNodeModel.java
@@ -1,17 +1,29 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
 package ie.tcd.imm.hits.image.omero.read;
 
-import ie.tcd.imm.hits.image.internal.ImagePlugin;
 import ie.tcd.imm.hits.util.Displayable;
+import ij.ImagePlus;
 
+import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 
+import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.RowKey;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
@@ -19,12 +31,12 @@ import org.knime.core.node.defaultnodesettings.SettingsModelEnum;
 import org.knime.core.node.defaultnodesettings.SettingsModelInteger;
 import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
-import org.openmicroscopy.shoola.env.Container;
-import org.openmicroscopy.shoola.env.init.BatchLoginServiceInit;
-import org.openmicroscopy.shoola.env.init.FakeInitializer;
-import org.openmicroscopy.shoola.env.init.LoginServiceInit;
-import org.openmicroscopy.shoola.env.init.NullSplashScreenInit;
-import org.openmicroscopy.shoola.env.init.SplashScreenInit;
+import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
+import org.knime.core.util.KnimeEncryption;
+import org.knime.exp.imaging.data.def.DefaultImageCell;
+
+import pojos.ImageData;
+import pojos.PixelsData;
 
 /**
  * This is the model implementation of OMEROReader. Allows to import data from
@@ -33,29 +45,36 @@ import org.openmicroscopy.shoola.env.init.SplashScreenInit;
  * @author <a href="mailto:bakosg@tcd.ie">Gabor Bakos</a>
  */
 public class OMEROReaderNodeModel extends NodeModel {
-	static {
-		String pluginPath;
-		try {
-			pluginPath = ImagePlugin.getDefault().getPluginPath()
-					.getAbsolutePath();
-		} catch (final IOException e) {
-			throw new RuntimeException(e);
-		}
-		FakeInitializer.replaceInitTask(LoginServiceInit.class,
-				BatchLoginServiceInit.class);
-		FakeInitializer.replaceInitTask(SplashScreenInit.class,
-				NullSplashScreenInit.class);
-		// Container.startup(pluginPath, "container.xml");
+	private static final String IMAGE_NAME = "image name";
+	private static final String IMAGE = "Image";
+	private static final String ID = "id";
+	static final NodeLogger logger = NodeLogger
+			.getLogger(OMEROReaderNodeModel.class);
 
-		final Container container = Container.startupInTestMode(pluginPath);
-		// final LoginService svc = (LoginService)
-		// container.getRegistry().lookup(
-		// LookupNames.LOGIN);
-		// final UserCredentials uc = new UserCredentials(userName, password,
-		// hostName, speedLevel);
-		// uc.setPort(port);
-		// svc.login(uc);
-	}
+	// static final Container container;
+	// static {
+	// String pluginPath;
+	// try {
+	// pluginPath = ImagePlugin.getDefault().getPluginPath()
+	// .getAbsolutePath();
+	// } catch (final IOException e) {
+	// throw new RuntimeException(e);
+	// }
+	// FakeInitializer.replaceInitTask(LoginServiceInit.class,
+	// BatchLoginServiceInit.class);
+	// FakeInitializer.replaceInitTask(SplashScreenInit.class,
+	// NullSplashScreenInit.class);
+	// // Container.startup(pluginPath, "container.xml");
+	//
+	// container = Container.startupInTestMode(pluginPath);
+	// // final LoginService svc = (LoginService)
+	// // container.getRegistry().lookup(
+	// // LookupNames.LOGIN);
+	// // final UserCredentials uc = new UserCredentials(userName, password,
+	// // hostName, speedLevel);
+	// // uc.setPort(port);
+	// // svc.login(uc);
+	// }
 
 	static enum ConnectionSpeed implements Displayable {
 		high {
@@ -80,26 +99,32 @@ public class OMEROReaderNodeModel extends NodeModel {
 
 	static final String CFGKEY_SERVER_NAME = "server.name";
 	static final String DEFAULT_SERVER_NAME = "ome2-copy.fzk.de";
-	static final String CFGKEY_SERVER_PORT = "server.name";
+	static final String CFGKEY_SERVER_PORT = "server.port";
 	static final int DEFAULT_SERVER_PORT = 4063;
 	static final String CFGKEY_CONNECTION_SPEED = "connection.speed";
 	static final ConnectionSpeed DEFAULT_CONNECTION_SPEED = ConnectionSpeed.low;
+	static final ConnectionSpeed[] POSSIBLE_CONNECTION_SPEEDS = ConnectionSpeed
+			.values();
 	static final String CFGKEY_USERNAME = "user.name";
 	static final String DEFAULT_USERNAME = "";
 	static final String CFGKEY_PASSWORD = "password";
 	static final String DEFAULT_PASSWORD = "";
+	static final String CFGKEY_SELECTION = "selection";
+	static final String[] DEFAULT_SELECTION = new String[] { "" };
 
 	private final SettingsModelString serverName = new SettingsModelString(
 			CFGKEY_SERVER_NAME, DEFAULT_SERVER_NAME);
 	private final SettingsModelInteger serverPort = new SettingsModelIntegerBounded(
 			CFGKEY_SERVER_PORT, DEFAULT_SERVER_PORT, 0, 65535);
 	private final SettingsModelEnum<ConnectionSpeed> connectionSpeed = new SettingsModelEnum<ConnectionSpeed>(
-			CFGKEY_CONNECTION_SPEED, DEFAULT_CONNECTION_SPEED, ConnectionSpeed
-					.values());
+			CFGKEY_CONNECTION_SPEED, DEFAULT_CONNECTION_SPEED,
+			POSSIBLE_CONNECTION_SPEEDS);
 	private final SettingsModelString userName = new SettingsModelString(
 			CFGKEY_USERNAME, DEFAULT_USERNAME);
 	private final SettingsModelString password = new SettingsModelString(
 			CFGKEY_PASSWORD, DEFAULT_PASSWORD);
+	private final SettingsModelStringArray selection = new SettingsModelStringArray(
+			CFGKEY_SELECTION, DEFAULT_SELECTION);
 
 	/**
 	 * Constructor for the node model.
@@ -114,8 +139,41 @@ public class OMEROReaderNodeModel extends NodeModel {
 	@Override
 	protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
 			final ExecutionContext exec) throws Exception {
-		// TODO: Return a BufferedDataTable for each output port
-		return new BufferedDataTable[] { null, null };
+		final DataTableSpec[] specs = configure(new DataTableSpec[0]);
+		final BufferedDataContainer images = exec.createDataContainer(specs[0]);
+		final OMEROHandler handler = OMEROHandler.getInstance();
+		handler.reconnect(serverName.getStringValue(),
+				serverPort.getIntValue(), userName.getStringValue(),
+				KnimeEncryption.decrypt(password.getStringValue()),
+				connectionSpeed.getEnumValue());
+		final long userId = handler.getExperimenterData().getId();
+		for (final String selectedString : selection.getStringArrayValue()) {
+			if (!selectedString.isEmpty()) {
+				assert selectedString.contains("\u00a0") : selectedString;
+				final long id = Long.parseLong(selectedString.substring(0,
+						selectedString.indexOf('\u00a0')));
+				handler.getDataService().getImages(ImageData.class,
+						Collections.singletonList(id), userId);
+				final PixelsData pixels = handler.getImageService().loadPixels(
+						id);
+				System.out.println(pixels.getPixelType());
+				System.out.println(pixels.getSizeC());
+				System.out.println(pixels.getSizeT());
+				System.out.println(pixels.getSizeX());
+				System.out.println(pixels.getSizeY());
+				System.out.println(pixels.getSizeZ());
+				images.addRowToTable(new DefaultRow(new RowKey(Long
+						.toString(id)), new IntCell((int) id), new StringCell(
+						selectedString.substring(selectedString
+								.indexOf('\u00a0') + 1)), new DefaultImageCell(
+						new ImagePlus((String) null, (Image) null))));
+			}
+		}
+		images.close();
+		final BufferedDataContainer metadata = exec
+				.createDataContainer(specs[1]);
+		metadata.close();
+		return new BufferedDataTable[] { images.getTable(), metadata.getTable() };
 	}
 
 	/**
@@ -132,9 +190,14 @@ public class OMEROReaderNodeModel extends NodeModel {
 	@Override
 	protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
 			throws InvalidSettingsException {
-
-		// TODO: generated method stub
-		return new DataTableSpec[] { null, null };
+		return new DataTableSpec[] {
+				new DataTableSpec(new DataColumnSpecCreator(ID, IntCell.TYPE)
+						.createSpec(), new DataColumnSpecCreator(IMAGE_NAME,
+						StringCell.TYPE).createSpec(),
+						new DataColumnSpecCreator(IMAGE, DefaultImageCell.TYPE)
+								.createSpec()),
+				new DataTableSpec(new DataColumnSpecCreator(ID, IntCell.TYPE)
+						.createSpec()) };
 	}
 
 	/**
@@ -147,6 +210,7 @@ public class OMEROReaderNodeModel extends NodeModel {
 		connectionSpeed.saveSettingsTo(settings);
 		userName.saveSettingsTo(settings);
 		password.saveSettingsTo(settings);
+		selection.saveSettingsTo(settings);
 	}
 
 	/**
@@ -160,6 +224,7 @@ public class OMEROReaderNodeModel extends NodeModel {
 		connectionSpeed.loadSettingsFrom(settings);
 		userName.loadSettingsFrom(settings);
 		password.loadSettingsFrom(settings);
+		selection.loadSettingsFrom(settings);
 	}
 
 	/**
@@ -173,6 +238,7 @@ public class OMEROReaderNodeModel extends NodeModel {
 		connectionSpeed.validateSettings(settings);
 		userName.validateSettings(settings);
 		password.validateSettings(settings);
+		selection.validateSettings(settings);
 	}
 
 	/**
