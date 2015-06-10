1b30e75a05c0186409ff996b5a3280093ea94237
aborgabor <aborgabor@gmail.com>
2015-06-10 16:49:00 +0000

    Moving tags to the proper location.

---
+++ ie.tcd.imm.hits/src/ie/tcd/imm/hits/util/swing/colour/Positions.java
@@ -0,0 +1,16 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
+package ie.tcd.imm.hits.util.swing.colour;
+
+/**
+ * The positions of the possible colours.
+ */
+public enum Positions {
+	/** The left/down position */
+	Down,
+	/** The middle position */
+	Middle,
+	/** The right/up position */
+	Up;
+}
