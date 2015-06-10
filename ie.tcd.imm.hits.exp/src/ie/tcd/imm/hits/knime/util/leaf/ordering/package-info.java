2b34f86b7ddee1456efb0b70990c94a2e5ff11ce
aborgabor <aborgabor@gmail.com>
2015-06-10 15:50:46 +0000

    Moving project to trunk.

---
+++ ie.tcd.imm.hits.exp/src/ie/tcd/imm/hits/knime/util/leaf/ordering/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
+/**
+ * Classes in this package allow to find an optimal reordering of the leafs of
+ * the result from <code>Hierarchical Clustering</code>.
+ */
+@Nonnull
+@CheckReturnValue
+package ie.tcd.imm.hits.knime.util.leaf.ordering;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
---
+++ ie.tcd.imm.hits.exp/src/ie/tcd/imm/hits/knime/util/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * All rights reserved. (C) Copyright 2009, Trinity College Dublin
+ */
+/**
+ * The classes in this package are sorting helping other classes in this project.
+ */
+@Nonnull
+@CheckReturnValue
+package ie.tcd.imm.hits.knime.util;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+
